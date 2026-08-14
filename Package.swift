201eaf5740951caff204a8f1722418bafba8409f
Noah Woodward <nhwoodward@gmail.com>
2026-08-14 15:44:20 -0400

    Support release builds with Swift 5.9 toolchains

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
@@ -17,6 +17,5 @@ let package = Package(
                 .linkedFramework("ServiceManagement"),
             ]
         ),
-    ],
-    swiftLanguageModes: [.v5]
+    ]
 )
